1afe129c2ef497c98d19839a9c66c5c3561ea397
Cook1619 <cook6348@gmail.com>
2020-08-08 14:44:08 -0500

    Logic complete for state

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import React, { Component } from "react";
-import Ball from "./Ball";
+import Lottery from "./Lottery";
 import "./App.css";
 
 export default class App extends Component {
   render() {
     return (
       <div className="App">
-        <Ball num="7" />
-        <Ball num="6" />
-        <Ball num="8" />
-        <Ball num="10" />
+        <Lottery />
       </div>
     );
   }
